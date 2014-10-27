b2531d3f2bc2376d8719b90fd4245a136c3260cb
Joshua Moerman <lakseru@gmail.com>
2014-10-27 17:38:23 +0100

    Converts more images to diagrams

--- thesis/test_diagram.sh
+++ thesis/test_diagram.sh
@@ -4,5 +4,5 @@ set -e
 file=$1
 sed "s|__INPUT__|$file|" test_diagram.tex | xelatex -file-line-error -output-directory=build
 mv build/texput.pdf test_diagram.pdf
-exo-open test_diagram.pdf
+#exo-open test_diagram.pdf
 rm build/texput.*
