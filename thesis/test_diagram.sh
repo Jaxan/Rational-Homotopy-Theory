6016bbd5c3ea71a41d3d7dc01e148bd1cbaebce3
Joshua Moerman <lakseru@gmail.com>
2015-04-05 20:33:37 +0200

    Restructures the files

--- thesis/test_diagram.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-file=$1
-sed "s|__INPUT__|$file|" test_diagram.tex | xelatex -file-line-error -output-directory=build
-mv build/texput.pdf test_diagram.pdf
-#exo-open test_diagram.pdf
-rm build/texput.*
